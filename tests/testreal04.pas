ce62db781d70357840124a30dd525302c3f07a55
otvin <michael_fredericks@hotmail.com>
2019-09-19 20:25:32 -0400

    Adding more compiler tests that compiler2 can now pass.

---
+++ tests/testreal04.pas
@@ -0,0 +1,9 @@
+program testreal04(output);
+var testreal:Real;
+
+begin
+    testreal := 1.2;
+    writeln(testreal + 2);
+    testreal := testreal * testreal;
+    writeln(testreal);
+end.
---
+++ tests/testreal07.pas
@@ -0,0 +1,8 @@
+program testreal07(output);
+
+begin
+    writeln(7.2/4.7);
+    writeln(4/2);
+    writeln(9/3.0);
+    writeln(9.0/3);
+end.
