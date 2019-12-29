111afc4821e3124195177684791fa26c153d095a
otvin <michael_fredericks@hotmail.com>
2019-12-29 00:36:39 -0500

    Refactored compiler_test to make it easier to maintain Fixed the FPC option Added displaying the expected vs. actual results for failed tests to save some steps when tests fail.

--- compiler_test.py
+++ compiler_test.py
@@ -22,7 +22,7 @@ def dotest(infilename, resultfilename):
         if not TEST_FPC_INSTEAD:
             compiler.compile(infilename, asmfilename=asmfilename, objfilename=objfilename, exefilename=exefilename)
         else:
-            os.system("fpc -v0 -o {0} {1}".format(exefilename, infilename))
+            os.system("fpc -v0 {}".format(infilename))
 
         os.system("./{0} > {1}".format(exefilename, testoutputfilename))
 
@@ -49,6 +49,10 @@ def dotest(infilename, resultfilename):
             os.system("rm {}".format(testoutputfilename))
         else:  # pragma: no cover
             print("FAIL: {}".format(infilename))
+            print("ACTUAL")
+            print(testvalue)
+            print("EXPECTED:")
+            print(resultvalue)
     except Exception as e:  # pragma: no cover
         print("FAIL: {}".format(infilename))
         print(e)
@@ -89,122 +93,82 @@ def do_compilefailtest(infilename, resultfilename):
             print(e)
 
 
+def generate_test_list(topic, start, end):
+    testlist = []
+    for i in range(start, end + 1):
+        test = topic
+        if i < 10:
+            test += "0"
+        test += str(i)
+        testlist.append(test)
+    return testlist
+
+
+def run_test_list(topic, start, end):
+    testlist = generate_test_list(topic, start, end)
+    for test in testlist:
+        pasfile = "tests/test" + test + ".pas"
+        outfile = "tests/test" + test + ".out"
+        dotest(pasfile, outfile)
+
+
+def run_compilefail_test_list(start, end):
+    testlist = generate_test_list("compilefail", start, end)
+    for test in testlist:
+        pasfile = "tests/" + test + ".pas"
+        outfile = "tests/" + test + ".out"
+        do_compilefailtest(pasfile, outfile)
+
+
 def main():
     global NUM_ATTEMPTS
     global NUM_SUCCESSES
 
-    dotest("tests/testassign01.pas", "tests/testassign01.out")
-    dotest("tests/testboolean01.pas", "tests/testboolean01.out")
-    dotest("tests/testbugfix01.pas", "tests/testbugfix01.out")
-    dotest("tests/testbyref01.pas", "tests/testbyref01.out")
-    dotest("tests/testbyref02.pas", "tests/testbyref02.out")
-    dotest("tests/testbyref03.pas", "tests/testbyref03.out")
-    dotest("tests/testbyref04.pas", "tests/testbyref04.out")
-    dotest("tests/testbyref05.pas", "tests/testbyref05.out")
-    dotest("tests/testcomments01.pas", "tests/testcomments01.out")
+    run_test_list("assign", 1, 1)
+    run_test_list("boolean", 1, 1)
+    run_test_list("bugfix", 1, 1)
+    run_test_list("byref", 1, 5)
+    run_test_list("comments", 1, 1)
+    run_test_list("divide", 1, 1)
+    run_test_list("functions", 1, 11)
+    run_test_list("functions", 13, 15)
+    run_test_list("globalvar", 1, 3)
+    run_test_list("idiv", 1, 2)
+    run_test_list("if", 1, 3)
+    run_test_list("localvar", 1, 2)
+    run_test_list("math", 1, 4)
+    run_test_list("misc", 1, 1)
+    run_test_list("mod", 1, 2)
+    # note testprocedure02 was "proc01" in the old compiler suite
+    run_test_list("procedure", 1, 7)
+    run_test_list("real", 1, 8)
+    run_test_list("recursion", 1, 1)
+    run_test_list("relop", 1, 4)
+    run_test_list("repeat", 1, 2)
+    run_test_list("scope", 1, 1)
+    run_test_list("scope", 3, 3)
+    run_test_list("sqrt", 1, 2)
+    run_test_list("while", 1, 3)
+    run_test_list("write", 1, 1)
+    run_test_list("writeln", 1, 3)
+
+    run_compilefail_test_list(1, 25)
+
+    # tests from old compiler not yet running in compiler2
     # dotest("tests/testconcat01.pas", "tests/testconcat01.out")
     # dotest("tests/testconcat02.pas", "tests/testconcat02.out")
     # dotest("tests/testconcat03.pas", "tests/testconcat03.out")
     # dotest("tests/testconcat04.pas", "tests/testconcat04.out")
     # dotest("tests/testconcat05.pas", "tests/testconcat05.out")
-    dotest("tests/testdivide01.pas", "tests/testdivide01.out")
-    dotest("tests/testfunctions01.pas", "tests/testfunctions01.out")
-    dotest("tests/testfunctions02.pas", "tests/testfunctions02.out")
-    dotest("tests/testfunctions03.pas", "tests/testfunctions03.out")
-    dotest("tests/testfunctions04.pas", "tests/testfunctions04.out")
-    dotest("tests/testfunctions05.pas", "tests/testfunctions05.out")
-    dotest("tests/testfunctions06.pas", "tests/testfunctions06.out")
-    dotest("tests/testfunctions07.pas", "tests/testfunctions07.out")
-    dotest("tests/testfunctions08.pas", "tests/testfunctions08.out")
-    dotest("tests/testfunctions09.pas", "tests/testfunctions09.out")
-    dotest("tests/testfunctions10.pas", "tests/testfunctions10.out")
-    dotest("tests/testfunctions11.pas", "tests/testfunctions11.out")
     # dotest("tests/testfunc12.pas", "tests/testfunc12.out")
-    dotest("tests/testfunctions13.pas", "tests/testfunctions13.out")
-    dotest("tests/testfunctions14.pas", "tests/testfunctions14.out")
-    dotest("tests/testfunctions15.pas", "tests/testfunctions15.out")
-    dotest("tests/testglobalvar01.pas", "tests/testglobalvar01.out")
-    dotest("tests/testglobalvar02.pas", "tests/testglobalvar02.out")
-    dotest("tests/testglobalvar03.pas", "tests/testglobalvar03.out")
-    dotest("tests/testidiv01.pas", "tests/testidiv01.out")
-    dotest("tests/testidiv02.pas", "tests/testidiv02.out")
-    dotest("tests/testif01.pas", "tests/testif01.out")
-    dotest("tests/testif02.pas", "tests/testif02.out")
-    dotest("tests/testif03.pas", "tests/testif03.out")
-    dotest("tests/testlocalvar01.pas", "tests/testlocalvar01.out")
-    dotest("tests/testlocalvar02.pas", "tests/testlocalvar02.out")
-    dotest("tests/testmath01.pas", "tests/testmath01.out")
-    dotest("tests/testmath02.pas", "tests/testmath02.out")
-    dotest("tests/testmath03.pas", "tests/testmath03.out")
-    dotest("tests/testmath04.pas", "tests/testmath04.out")
-    dotest("tests/testmisc01.pas", "tests/testmisc01.out")
-    dotest("tests/testmod01.pas", "tests/testmod01.out")
-    dotest("tests/testmod02.pas", "tests/testmod02.out")
     # dotest("tests/testproc02.pas", "tests/testproc02.out")
     # dotest("tests/testproc03.pas", "tests/testproc03.out")
     # dotest("tests/testproc04.pas", "tests/testproc04.out")
-    dotest("tests/testprocedure01.pas", "tests/testprocedure01.out")
-    dotest("tests/testprocedure02.pas", "tests/testprocedure02.out") # was testproc01.pas in the "compiler" suite
-    dotest("tests/testprocedure03.pas", "tests/testprocedure03.out")
-    dotest("tests/testprocedure04.pas", "tests/testprocedure04.out")
-    dotest("tests/testprocedure05.pas", "tests/testprocedure05.out")
-    dotest("tests/testprocedure06.pas", "tests/testprocedure06.out")
-    dotest("tests/testprocedure07.pas", "tests/testprocedure07.out")
-    dotest("tests/testreal01.pas", "tests/testreal01.out")
-    dotest("tests/testreal02.pas", "tests/testreal02.out")
-    dotest("tests/testreal03.pas", "tests/testreal03.out")
-    dotest("tests/testreal04.pas", "tests/testreal04.out")
-    dotest("tests/testreal05.pas", "tests/testreal05.out")
-    dotest("tests/testreal06.pas", "tests/testreal06.out")
-    dotest("tests/testreal07.pas", "tests/testreal07.out")
-    dotest("tests/testreal08.pas", "tests/testreal08.out")
-    dotest("tests/testrecursion01.pas", "tests/testrecursion01.out")
-    dotest("tests/testrelop01.pas", "tests/testrelop01.out")
-    dotest("tests/testrelop02.pas", "tests/testrelop02.out")
-    dotest("tests/testrelop03.pas", "tests/testrelop03.out")
-    dotest("tests/testrelop04.pas", "tests/testrelop04.out")
-    dotest("tests/testrepeat01.pas", "tests/testrepeat01.out")
-    dotest("tests/testrepeat02.pas", "tests/testrepeat02.out")
-    dotest("tests/testscope01.pas", "tests/testscope01.out")
     # dotest("tests/testscope02.pas", "tests/testscope02.out")
-    dotest("tests/testscope03.pas", "tests/testscope03.out")
     # dotest("tests/teststring01.pas", "tests/teststring01.out")
     # dotest("tests/teststring02.pas", "tests/teststring02.out")
     # dotest("tests/teststring03.pas", "tests/teststring03.out")
     # dotest("tests/teststring04.pas", "tests/teststring04.out")
-    dotest("tests/testwhile01.pas", "tests/testwhile01.out")
-    dotest("tests/testwhile02.pas", "tests/testwhile02.out")
-    dotest("tests/testwhile03.pas", "tests/testwhile03.out")
-    dotest("tests/testwrite01.pas", "tests/testwrite01.out")
-    dotest("tests/testwriteln01.pas", "tests/testwriteln01.out")
-    dotest("tests/testwriteln02.pas", "tests/testwriteln02.out")
-    dotest("tests/testwriteln03.pas", "tests/testwriteln03.out")
-
-    do_compilefailtest("tests/compilefail01.pas", "tests/compilefail01.out")
-    do_compilefailtest("tests/compilefail02.pas", "tests/compilefail02.out")
-    do_compilefailtest("tests/compilefail03.pas", "tests/compilefail03.out")
-    do_compilefailtest("tests/compilefail04.pas", "tests/compilefail04.out")
-    do_compilefailtest("tests/compilefail05.pas", "tests/compilefail05.out")
-    do_compilefailtest("tests/compilefail06.pas", "tests/compilefail06.out")
-    do_compilefailtest("tests/compilefail07.pas", "tests/compilefail07.out")
-    do_compilefailtest("tests/compilefail08.pas", "tests/compilefail08.out")
-    do_compilefailtest("tests/compilefail09.pas", "tests/compilefail09.out")
-    do_compilefailtest("tests/compilefail10.pas", "tests/compilefail10.out")
-    do_compilefailtest("tests/compilefail11.pas", "tests/compilefail11.out")
-    do_compilefailtest("tests/compilefail12.pas", "tests/compilefail12.out")
-    do_compilefailtest("tests/compilefail13.pas", "tests/compilefail13.out")
-    do_compilefailtest("tests/compilefail14.pas", "tests/compilefail14.out")
-    do_compilefailtest("tests/compilefail15.pas", "tests/compilefail15.out")
-    do_compilefailtest("tests/compilefail16.pas", "tests/compilefail16.out")
-    do_compilefailtest("tests/compilefail17.pas", "tests/compilefail17.out")
-    do_compilefailtest("tests/compilefail18.pas", "tests/compilefail18.out")
-    do_compilefailtest("tests/compilefail19.pas", "tests/compilefail19.out")
-    do_compilefailtest("tests/compilefail20.pas", "tests/compilefail20.out")
-    do_compilefailtest("tests/compilefail21.pas", "tests/compilefail21.out")
-    do_compilefailtest("tests/compilefail22.pas", "tests/compilefail22.out")
-    do_compilefailtest("tests/compilefail23.pas", "tests/compilefail23.out")
-    do_compilefailtest("tests/compilefail24.pas", "tests/compilefail24.out")
-    do_compilefailtest("tests/compilefail25.pas", "tests/compilefail25.out")
 
     print("Tests Attempted: " + str(NUM_ATTEMPTS))
     print("Tests Succeeded: " + str(NUM_SUCCESSES))
